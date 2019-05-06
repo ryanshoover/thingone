1e7c47e75d37bc6622e2810485955719a81e5e57
Ryan Hoover <ryanshoover@gmail.com>
2019-05-05 21:22:36 -0500

    Initial setup

---
+++ tests/basicConfig.js
@@ -0,0 +1,19 @@
+const baseUrl = "https://thingone.wpengine.com";
+const projectId = "Thing One";
+
+const relativeUrls =[
+	"/",
+];
+
+const viewports = [
+	"phone",
+	"tablet",
+"desktop",
+];
+
+module.exports = {
+	baseUrl,
+	projectId,
+	relativeUrls,
+	viewports,
+};
